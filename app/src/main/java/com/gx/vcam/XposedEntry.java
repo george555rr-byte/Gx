6abb674191a7de4a8dfdacc87bb9a0ae8160455d
george555rr-byte <george555rr@gmail.com>
2026-09-15 02:40:51 +0300

    Refactor handleLoadPackage method in XposedEntry

--- app/src/main/java/com/gx/vcam/XposedEntry.java
+++ app/src/main/java/com/gx/vcam/XposedEntry.java
@@ -4,14 +4,8 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedEntry implements IXposedHookLoadPackage {
-
-    @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        try {
-            if ("com.gx.vcam".equals(lpparam.packageName)) return;
-            new HookMain().handleLoadPackage(lpparam);
-        } catch (Throwable t) {
-            GxLog.w("GX", "entry err: " + t);
-        }
+    @Override public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lp) {
+        try { if (!"com.gx.vcam".equals(lp.packageName)) new GxHook().handleLoadPackage(lp); }
+        catch (Throwable t) { GxLog.w("GX", "entry " + t); }
     }
 }
