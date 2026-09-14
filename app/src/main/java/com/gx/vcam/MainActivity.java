f3cdc27d229eab91fb49dbffbbe3824d955a1ab4
george555rr-byte <george555rr@gmail.com>
2026-09-14 19:37:31 +0300

    Create MainActivity class for the app

---
+++ app/src/main/java/com/gx/vcam/MainActivity.java
@@ -0,0 +1,9 @@
+package com.gx.vcam;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+}
