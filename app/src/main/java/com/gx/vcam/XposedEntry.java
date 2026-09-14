672bf403917e297803f9dbc87b96a18a926c5937
george555rr-byte <george555rr@gmail.com>
2026-09-14 19:38:41 +0300

    Add XposedEntry class for package hooking

---
+++ app/src/main/java/com/gx/vcam/XposedEntry.java
@@ -0,0 +1,7 @@
+package com.gx.vcam;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+public class XposedEntry implements IXposedHookLoadPackage {
+    @Override public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+    }
+}
