d06df8dd193ffcc4dfc4ffc5f6c66cabf022a0d5
george555rr-byte <george555rr@gmail.com>
2026-09-15 21:52:10 +0300

    Update XposedEntry.java

--- app/src/main/java/com/gx/vcam/XposedEntry.java
+++ app/src/main/java/com/gx/vcam/XposedEntry.java
@@ -5,7 +5,9 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedEntry implements IXposedHookLoadPackage {
     @Override public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lp) {
-        try { if (!"com.gx.vcam".equals(lp.packageName)) new GxHook().handleLoadPackage(lp); }
-        catch (Throwable t) { GxLog.w("GX", "entry " + t); }
+        try {
+            if (!"com.gx.vcam".equals(lp.packageName))
+                new HookMain().handleLoadPackage(lp);
+        } catch (Throwable t) { GxLog.w("GX", "entry " + t); }
     }
 }
